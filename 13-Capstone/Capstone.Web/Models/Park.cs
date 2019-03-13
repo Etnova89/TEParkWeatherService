37388c22293b7d0db521987c63ede2285ab21682
Grant Payne <afroman7600@yahoo.com>
2019-03-13 11:27:11 -0400

    completed parkDAL

--- 13-Capstone/Capstone.Web/Models/Park.cs
+++ 13-Capstone/Capstone.Web/Models/Park.cs
@@ -10,12 +10,13 @@ namespace Capstone.Web.Models
         public string ParkCode { get; set; }
         public string ParkName { get; set; }
         public string State { get; set; }
-        public string Acreage { get; set; }
+        public int Acreage { get; set; }
         public int ElevationInFeet { get; set; }
         public double MilesOfTrail { get; set; }
+        public double NumberOfCampsites { get; set; }
         public string Climate { get; set; }
         public int YearFounded { get; set; }
-        public string AnnualVisitorCount { get; set; }
+        public int AnnualVisitorCount { get; set; }
         public string InspirationalQuote { get; set; }
         public string InspirationalQuoteSource { get; set; }
         public string ParkDescription { get; set; }
