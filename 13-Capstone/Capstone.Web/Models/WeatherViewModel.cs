490e26b2fdf9fd97aa86127ced1c81c86b4b3340
Eric Terranova <terranova0689@gmail.com>
2019-03-13 10:49:34 -0400

    added WeatherViewModel

---
+++ 13-Capstone/Capstone.Web/Models/WeatherViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Web.Models
+{
+    public class WeatherViewModel
+    {
+        public double ConvertTempToC(bool result)
+        {
+            double convertedTemp = 0;
+
+            return convertedTemp;
+        }
+    }
+}
