c35e07a47fbab51cb5620e393ebcfec88d68f283
Evan Williamson <EvanTroyWilliamson@gmail.com>
2019-03-13 12:59:57 -0400

    Finished one method for SurveySqlDAL

--- 13-Capstone/Capstone.Web/DAL/Interfaces/ISurveySqlDAL.cs
+++ 13-Capstone/Capstone.Web/DAL/Interfaces/ISurveySqlDAL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Capstone.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,7 @@ namespace Capstone.Web.DAL.Interface
 {
     public interface ISurveySqlDAL
     {
-        
+        Survey GetSurvey();
+        bool SubmitSurvey(Survey survey);
     }
 }
---
+++ 13-Capstone/Capstone.Web/DAL/SurveySqlDAL.cs
@@ -0,0 +1,55 @@
+﻿using Capstone.Web.DAL.Interface;
+using Capstone.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Web.DAL
+{
+    public class SurveySqlDAL : ISurveySqlDAL
+    {
+        private string connectionString;
+        private string SQL_GetSurverys = @"";
+        private string SQL_SubmitSurvey = @"INSERT INTO survey_result (parkCode, emailAddress, state, activityLevel) VALUES (@parkCode, @emailAddress, @state, @activityLevel)";
+
+        public SurveySqlDAL(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public Survey GetSurvey()
+        {
+            Survey survey = new Survey();
+            return survey;
+        }
+
+        public bool SubmitSurvey(Survey survey)
+        {
+            bool result = false;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    SqlCommand cmd = new SqlCommand(SQL_SubmitSurvey, connection);
+                    cmd.Parameters.AddWithValue("@parkCode", survey.ParkCode);
+                    cmd.Parameters.AddWithValue("@emailAddress", survey.EmailAddress);
+                    cmd.Parameters.AddWithValue("@state", survey.ParkCode);
+                    cmd.Parameters.AddWithValue("@activityLevel", survey.ActivityLevel);
+                    if (cmd.ExecuteNonQuery() > 0)
+                    {
+                        result = true;
+                    }
+                }
+            }
+            catch
+            {
+                throw;
+            }
+
+            return result;
+
+        }
+    }
+}
