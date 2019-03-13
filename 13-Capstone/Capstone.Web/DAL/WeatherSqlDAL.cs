7a2be63e1d124040eb9f057aa406a4da4fe9803a
Grant Payne <afroman7600@yahoo.com>
2019-03-13 14:16:25 -0400

    completed weatherSqlDAL

--- 13-Capstone/Capstone.Web/DAL/WeatherSqlDAL.cs
+++ 13-Capstone/Capstone.Web/DAL/WeatherSqlDAL.cs
@@ -1,11 +1,57 @@
-﻿using System;
+﻿using Capstone.Web.DAL.Interfaces;
+using Capstone.Web.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Capstone.Web.DAL
 {
-    public class WeatherSqlDAL
+    public class WeatherSqlDAL : IWeatherSqlDAL
     {
+        private string connectionString;
+
+        private const string SQL_Get_Weather_By_Park = "SELECT TOP 5 * FROM weather WHERE parkCode = @parkCode ORDER BY fiveDayForcastValue";
+
+        public WeatherSqlDAL(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public List<Weather> GetWeatherByParkCode(string parkCode)
+        {
+            List<Weather> weatherList = new List<Weather>();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand(SQL_Get_Weather_By_Park, conn);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Weather weather = new Weather();
+
+                        weather.ParkCode = Convert.ToString(reader["parkCode"]);
+                        weather.FiveDayForecastValue = Convert.ToInt32(reader["fiveDayForecastValue"]);
+                        weather.Low = Convert.ToInt32(reader["low"]);
+                        weather.High = Convert.ToInt32(reader["high"]);
+                        weather.Forecast = Convert.ToString(reader["forecast"]);
+
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return weatherList;
+        }
+
     }
 }
