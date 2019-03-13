fd89081bc73843559f1c2bdec8ec425a081cf788
Eric Terranova <terranova0689@gmail.com>
2019-03-13 10:39:41 -0400

    models

---
+++ 13-Capstone/Capstone.Web/Models/Park.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Web.Models
+{
+    public class Park
+    {
+        public string ParkCode { get; set; }
+        public string ParkName { get; set; }
+        public string State { get; set; }
+        public string Acreage { get; set; }
+        public int ElevationInFeet { get; set; }
+        public double MilesOfTrail { get; set; }
+        public string Climate { get; set; }
+        public int YearFounded { get; set; }
+        public string AnnualVisitorCount { get; set; }
+        public string InspirationalQuote { get; set; }
+        public string InspirationalQuoteSource { get; set; }
+        public string ParkDescription { get; set; }
+        public int EntryFee { get; set; }
+        public int NumberOfAnimalSpecies { get; set; }
+    }
+}
---
+++ 13-Capstone/Capstone.Web/Models/Survey.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Web.Models
+{
+    public class Survey
+    {
+        public int SurveyId { get; set; }
+        public string ParkCode { get; set; }
+        public string EmailAddress { get; set; }
+        public string State { get; set; }
+        public string ActivityLevel { get; set; }
+    }
+}
